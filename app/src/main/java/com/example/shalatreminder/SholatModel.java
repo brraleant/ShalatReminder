0e23dbda1b652882c307aec36a6edd8081c69efa
brraleant <aliapute@gmail.com>
2019-04-05 22:02:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/shalatreminder/SholatModel.java
@@ -0,0 +1,23 @@
+package com.example.shalatreminder;
+
+public class SholatModel {
+    String name, date, time;
+
+    public SholatModel(){
+        this.name = name;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
